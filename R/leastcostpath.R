dc9e2cb5b8735be864db7bcedcc7264183556212
josephlewis <jl557@le.ac.uk>
2018-12-27 21:39:11 +0000

    update to include horizontal factor

--- R/leastcostpath.R
+++ R/leastcostpath.R
@@ -1,10 +1,10 @@
-leastcostpath <- function(dem, origin, destination, cost_function = "all", direction = FALSE, neighbours = 16, crit_slope = 15) {
+leastcostpath <- function(dem, origin, destination, cost_function = "all", direction = FALSE, neighbours = 16, crit_slope = 15, hf = "linear", linear_values = c(-2/180, 2), binary_values = c(45, 999, 1), forward_values = c(999, 1, 0.5)) {
     
-    altDiff <- function(x) {
+    altDiff_slope <- function(x) {
         x[2] - x[1]
     }
     
-    hd <- gdistance::transition(dem, altDiff, 8)
+    hd <- gdistance::transition(dem, altDiff_slope, 8)
     
     slope <- gdistance::geoCorrection(hd)
     
@@ -19,6 +19,58 @@ leastcostpath <- function(dem, origin, destination, cost_function = "all", direc
         Conductance <- stack(gdistance::geoCorrection(slope_stack[[1]]), gdistance::geoCorrection(slope_stack[[2]]), gdistance::geoCorrection(slope_stack[[3]]))
     }
     
+    aspect <- raster::terrain(dem, opt = "aspect", unit = "degrees", neighbors = 8)
+    
+    altDiff_aspect <- function(x) {
+        if (hf == "linear") {
+            if (abs(x[2] - x[1]) > 180) {
+                hrma <- abs(x[2] - x[1]) - 180
+                (linear_values[1]) * hrma + linear_values[2]
+            } else {
+                hrma <- abs(x[2] - x[1])
+                (linear_values[1]) * hrma + linear_values[2]
+            }
+        } else if (hf == "binary") {
+            if (abs(x[2] - x[1]) > 180) {
+                hrma <- abs(x[2] - x[1]) - 180
+                ifelse(hrma > binary_values[1], binary_values[3], binary_values[2])
+            } else {
+                hrma <- abs(x[2] - x[1])
+                ifelse(hrma > binary_values[1], binary_values[3], binary_values[2])
+            }
+        } else if (hf == "forward") {
+            if (abs(x[2] - x[1]) > 180) {
+                hrma <- abs(x[2] - x[1]) - 180
+                if (hrma < 45) {
+                  forward_values[1]
+                } else if (hrma >= 45 & hrma < 90) {
+                  forward_values[2]
+                } else {
+                  forward_values[3]
+                }
+            } else {
+                hrma <- abs(x[2] - x[1])
+                if (hrma < 45) {
+                  forward_values[1]
+                } else if (hrma >= 45 & hrma < 90) {
+                  forward_values[2]
+                } else {
+                  forward_values[3]
+                }
+            }
+        }
+    }
+    
+    
+    trans <- gdistance::transition(aspect, altDiff_aspect, 16)
+    
+    Conductance[[1]] <- Conductance[[1]] * trans
+    Conductance[[2]] <- Conductance[[2]] * trans
+    Conductance[[3]] <- Conductance[[3]] * trans
+    
+    plot(raster(Conductance[[1]]))
+    plot(raster(Conductance[[2]]))
+    plot(raster(Conductance[[3]]))
     
     if (inherits(origin, "SpatialPoints") & inherits(destination, "SpatialPoints")) {
         sPath <- list()
@@ -64,7 +116,8 @@ leastcostpath <- function(dem, origin, destination, cost_function = "all", direc
     }
     
     for (i in 1:length(sPath)) {
-        rgdal::writeOGR(sPath[[i]], ".", paste0(names(sPath)[i], "_", format(Sys.time(), "%d-%b-%Y %H.%M")), driver = "ESRI Shapefile", overwrite_layer = TRUE)
+        rgdal::writeOGR(sPath[[i]], ".", paste0(names(sPath)[i], "_", hf), driver = "ESRI Shapefile", overwrite_layer = TRUE)
+        
     }
     
 }
