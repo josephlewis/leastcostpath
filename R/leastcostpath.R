4237897fb401b4341dae5356b8ff6818f4586d4b
josephlewis <jl557@le.ac.uk>
2018-12-28 18:54:44 +0000

    Add files via upload

--- R/leastcostpath.R
+++ R/leastcostpath.R
@@ -1,26 +1,27 @@
-leastcostpath <- function(dem, origin, destination, cost_function = "all", direction = FALSE, neighbours = 16, crit_slope = 15, hf = "linear", linear_values = c(-2/180, 2), binary_values = c(45, 999, 1), forward_values = c(999, 1, 0.5)) {
-    
+leastcostpath <- function(dem, origin, destination, cost_function = "all", direction = FALSE, neighbours = 16, crit_slope = 15, hf = "linear", linear_values = c(-2/180,
+    2), binary_values = c(45, 999, 1), forward_values = c(999, 1, 0.5)) {
+
     altDiff_slope <- function(x) {
         x[2] - x[1]
     }
-    
+
     hd <- gdistance::transition(dem, altDiff_slope, 8)
-    
+
     slope <- gdistance::geoCorrection(hd)
-    
+
     if (cost_function == "all") {
-        adj <- raster::adjacent(dem, cells = 1:ncell(dem), pairs = TRUE, directions = neighbours)
+        adj <- raster::adjacent(dem, cells = 1:raster::ncell(dem), pairs = TRUE, directions = neighbours)
         slope_stack <- raster::stack(slope, slope, slope)
-        
+
         slope_stack[[1]][adj] <- 6 * exp(-3.5 * abs(slope[adj] + 0.05))
         slope_stack[[2]][adj] <- 4.8 * exp(-5.3 * abs(slope[adj] * 0.7) + 0.03)
         slope_stack[[3]][adj] <- 1/(1 + abs(slope[adj]/crit_slope)^2)
-        
-        Conductance <- stack(gdistance::geoCorrection(slope_stack[[1]]), gdistance::geoCorrection(slope_stack[[2]]), gdistance::geoCorrection(slope_stack[[3]]))
+
+        Conductance <- raster::stack(gdistance::geoCorrection(slope_stack[[1]]), gdistance::geoCorrection(slope_stack[[2]]), gdistance::geoCorrection(slope_stack[[3]]))
     }
-    
+
     aspect <- raster::terrain(dem, opt = "aspect", unit = "degrees", neighbors = 8)
-    
+
     altDiff_aspect <- function(x) {
         if (hf == "linear") {
             if (abs(x[2] - x[1]) > 180) {
@@ -60,18 +61,14 @@ leastcostpath <- function(dem, origin, destination, cost_function = "all", direc
             }
         }
     }
-    
-    
+
+
     trans <- gdistance::transition(aspect, altDiff_aspect, 16)
-    
+
     Conductance[[1]] <- Conductance[[1]] * trans
     Conductance[[2]] <- Conductance[[2]] * trans
     Conductance[[3]] <- Conductance[[3]] * trans
-    
-    plot(raster(Conductance[[1]]))
-    plot(raster(Conductance[[2]]))
-    plot(raster(Conductance[[3]]))
-    
+
     if (inherits(origin, "SpatialPoints") & inherits(destination, "SpatialPoints")) {
         sPath <- list()
         origin <- sp::coordinates(origin)
@@ -79,45 +76,45 @@ leastcostpath <- function(dem, origin, destination, cost_function = "all", direc
     } else {
         return("Origin or Destination is not a SpatialPoints")
     }
-    
+
     if (direction == "TRUE") {
-        
+
         sPath[[1]] <- gdistance::shortestPath(Conductance[[1]], origin, destination, output = "SpatialLines")
-        
+
         sPath[[2]] <- gdistance::shortestPath(Conductance[[2]], origin, destination, output = "SpatialLines")
-        
+
         sPath[[3]] <- gdistance::shortestPath(Conductance[[3]], origin, destination, output = "SpatialLines")
-        
+
         names(sPath) <- c("Toblers A to B", "Marquez-Perez A to B", "Lloberas A to B")
-        
+
     } else {
-        
+
         sPath[[1]] <- gdistance::shortestPath(Conductance[[1]], origin, destination, output = "SpatialLines")
-        
+
         sPath[[2]] <- gdistance::shortestPath(Conductance[[1]], destination, origin, output = "SpatialLines")
-        
+
         sPath[[3]] <- gdistance::shortestPath(Conductance[[2]], origin, destination, output = "SpatialLines")
-        
+
         sPath[[4]] <- gdistance::shortestPath(Conductance[[2]], destination, origin, output = "SpatialLines")
-        
+
         sPath[[5]] <- gdistance::shortestPath(Conductance[[3]], origin, destination, output = "SpatialLines")
-        
+
         names(sPath) <- c("Toblers A to B", "Toblers B to A", "Marquez-Perez A to B", "Marquez-Perez B to A", "Lloberas A to B")
-        
+
     }
-    
-    
+
+
     lcp_lengths <- unlist(lapply(sPath, function(x) {
         FUN = rgeos::gLength(x, byid = TRUE)
     }))
-    
+
     for (i in 1:length(sPath)) {
         sPath[[i]]$length <- lcp_lengths[i]
     }
-    
+
     for (i in 1:length(sPath)) {
         rgdal::writeOGR(sPath[[i]], ".", paste0(names(sPath)[i], "_", hf), driver = "ESRI Shapefile", overwrite_layer = TRUE)
-        
+
     }
-    
+
 }
