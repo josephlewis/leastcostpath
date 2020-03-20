1d3b587904fbbbd12c89b5e10ab24d4676c7dee7
Joseph <Josephlewis1992@gmail.com>
2020-03-20 15:43:53 +0000

    for CRAN

--- R/create_FETE_lcps.R
+++ R/create_FETE_lcps.R
@@ -37,58 +37,58 @@
 #' cost_distance = FALSE, parallel = FALSE)
 
 create_FETE_lcps <- function(cost_surface, locations, cost_distance = FALSE, parallel = FALSE) {
-
+    
     if (!inherits(cost_surface, "TransitionLayer")) {
         stop("cost_surface argument is invalid. Expecting a TransitionLayer object")
     }
-
+    
     if (!inherits(locations, c("SpatialPoints", "SpatialPointsDataFrame"))) {
         stop("Locations argument is invalid. Expecting SpatialPoints or SpatialPointsDataFrame object")
     }
-
-    if (length(locations) < 2)
+    
+    if (length(locations) < 2) 
         stop("Number of locations invalid. Expecting more than one location")
-
+    
     network <- (expand.grid(seq_along(locations), seq_along(locations)))
-
+    
     network <- network[network[, 1] != network[, 2], ]
-
+    
     if (parallel) {
-
+        
         no_cores <- parallel::detectCores() - 1
-
+        
         cl <- parallel::makeCluster(no_cores)
-
+        
         parallel::clusterExport(cl, varlist = c("cost_surface", "locations"), envir = environment())
-
+        
         lcp_network <- pbapply::pbapply(network, MARGIN = 1, function(x) {
             gdistance::shortestPath(cost_surface, locations[x[1], ], locations[x[2], ], output = "SpatialLines")
         }, cl = cl)
-
+        
         parallel::stopCluster(cl)
-
+        
     } else {
-
+        
         lcp_network <- pbapply::pbapply(network, MARGIN = 1, function(x) {
             gdistance::shortestPath(cost_surface, locations[x[1], ], locations[x[2], ], output = "SpatialLines")
         })
-
+        
     }
-
+    
     lcp_network <- do.call(rbind, lcp_network)
-
+    
     lcp_network <- SpatialLinesDataFrame(lcp_network, data.frame(from = network[, 1], to = network[, 2]), match.ID = FALSE)
-
+    
     if (cost_distance) {
-
+        
         cost_dist <- apply(network, MARGIN = 1, function(x) {
             gdistance::costDistance(cost_surface, locations[x[1], ], locations[x[2], ])
         })
-
+        
         lcp_network$cost <- cost_dist
-
+        
     }
-
+    
     return(lcp_network)
-
+    
 }
