3fd25253cfd9af24114dbc62bfdd95e7db2fce42
Joseph <Josephlewis1992@gmail.com>
2020-04-28 16:53:02 +0100

    added cost functions

--- R/create_slope_cs.R
+++ R/create_slope_cs.R
@@ -6,9 +6,11 @@
 #'
 #' @details
 #'
-#' The most widely used cost function that approximates the difficulty of moving across a landscape is Waldo Tobler's 'Hiking Function' (Gorenflo and Gale, 1990; Wheatley and Gillings, 2001). This function allows for the assessment of time necessary to traverse a surface and takes into account up-slope and down-slope momentum (Kantner, 2004; Tobler, 1993).
+#' Tobler's 'Hiking Function' is the most widely used cost function when approximating the difficulty of moving across a landscape (Gorenflo and Gale, 1990; Wheatley and Gillings, 2001). The function assess the time necessary to traverse a surface and takes into account up-slope and down-slope (Kantner, 2004; Tobler, 1993).
 #'
-#' Tobler's offpath cost function reduces speed of the Tobler's 'Hiking function' by 0.6 to take into account walking off-path (Tobler, 1993)
+#' Tobler's offpath Hiking Function reduces the speed of the Tobler's Hiking Function by 0.6 to take into account walking off-path (Tobler, 1993)
+#'
+#'The Irmischer and Clark functions were modelled from speed estimates of United States Military Academy (USMA) cadets while they navigated on foot over hilly, wooded terrain as part of their summer training in map and compass navigation.
 #'
 #' The Modified Hiking cost function combines MIDE (París Roche, 2002), a method to calculate walking hours for an average hiker with a light load (Márquez-Pérez et al., 2017), and Tobler's 'Hiking Function' (Tobler, 1993). The Modified Hiking Function benefits from the precision of the MIDE rule and the continuity of Tobler's Hiking Function (Márquez-Pérez et al., 2017).
 #'
@@ -16,11 +18,11 @@
 #'
 #' Herzog's (2010) Sixth-degree polynomial cost function approximates the energy expenditure values found in Minetti et al. (2002) but eliminates the problem of unrealistic negative energy expenditure values for steep downhill slopes.
 #'
-#' Llobera and Sluckin (2007) metabolic energy expenditure cost function in KJ/(m*kg)
+#' Llobera and Sluckin (2007) cost function approximates the metabolic energy expenditure in KJ/(m*kg) when moving across a landscape.
 #'
 #' @param dem \code{RasterLayer} (raster package). Digital Elevation Model.
 #'
-#' @param cost_function Cost Function used in the Least Cost Path calculation. Implemented cost functions include 'tobler', 'tobler offpath', 'modified tobler', 'wheeled transport' 'herzog', 'llobera_sluckin' or 'all'. Default is 'tobler'. See Details for more information.
+#' @param cost_function Cost Function used in the Least Cost Path calculation. Implemented cost functions include 'tobler', 'tobler offpath', 'irmischer-clarke male', 'irmischer-clarke offpath male', 'irmischer-clarke female', 'irmischer-clarke offpath female', 'modified tobler', 'wheeled transport', 'herzog', 'llobera-sluckin', 'all'. Default is 'tobler'. See Details for more information.
 #'
 #' @param neighbours \code{numeric} value. Number of directions used in the Least Cost Path calculation. See Huber and Church (1985) for methodological considerations when choosing number of neighbours. Expected values are 4, 8, or 16. Default is 16.
 #'
@@ -51,7 +53,8 @@ create_slope_cs <- function(dem, cost_function = "tobler", neighbours = 16, crit
         stop("dem argument is invalid. Expecting a RasterLayer object")
     }
     
-    cfs <- c("tobler", "tobler offpath", "modified tobler", "wheeled transport", "herzog", "llobera_sluckin", "all")
+    cfs <- c("tobler", "tobler offpath", "irmischer-clarke male", "irmischer-clarke offpath male", "irmischer-clarke female", "irmischer-clarke offpath female", "modified tobler", 
+        "wheeled transport", "herzog", "llobera-sluckin", "all")
     
     if (!cost_function %in% cfs) {
         stop("cost_function argument is invalid. See details for accepted cost functions")
@@ -99,6 +102,38 @@ create_slope_cs <- function(dem, cost_function = "tobler", neighbours = 16, crit
         
     }
     
+    if (cost_function == "irmischer-clarke male") {
+        
+        cf <- function(x) {
+            (0.11 + exp(-(abs(x[adj]) * 100 + 5)^2/(2 * 30^2)))
+        }
+        
+    }
+    
+    if (cost_function == "irmischer-clarke offpath male") {
+        
+        cf <- function(x) {
+            (0.11 + 0.67 * exp(-(abs(x[adj]) * 100 + 2)^2/(2 * 30^2)))
+        }
+        
+    }
+    
+    if (cost_function == "irmischer-clarke female") {
+        
+        cf <- function(x) {
+            0.95 * (0.11 + exp(-(abs(x[adj]) * 100 + 5)^2/(2 * 30^2)))
+        }
+        
+    }
+    
+    if (cost_function == "irmischer-clarke offpath female") {
+        
+        cf <- function(x) {
+            0.95 * (0.11 + 0.67 * exp(-(abs(x[adj]) * 100 + 2)^2/(2 * 30^2)))
+        }
+        
+    }
+    
     if (cost_function == "modified tobler") {
         
         cf <- function(x) {
@@ -123,7 +158,7 @@ create_slope_cs <- function(dem, cost_function = "tobler", neighbours = 16, crit
         
     }
     
-    if (cost_function == "llobera_sluckin") {
+    if (cost_function == "llobera-sluckin") {
         
         cf <- function(x) {
             (1/(2.635 + (17.37 * abs(x[adj])) + (42.37 * abs(x[adj])^2) - (21.43 * abs(x[adj])^3) + (14.93 * abs(x[adj])^4)))
@@ -145,7 +180,7 @@ create_slope_cs <- function(dem, cost_function = "tobler", neighbours = 16, crit
     
     if (cost_function %in% cfs[cfs == "all"]) {
         
-        slope_stack <- list(slope, slope, slope, slope, slope, slope)
+        slope_stack <- list(slope, slope, slope, slope, slope, slope, slope, slope, slope, slope)
         
         cf1 <- function(x) {
             (6 * exp(-3.5 * abs(x[adj] + 0.05)))
@@ -153,16 +188,33 @@ create_slope_cs <- function(dem, cost_function = "tobler", neighbours = 16, crit
         cf2 <- function(x) {
             (6 * exp(-3.5 * abs(x[adj] + 0.05))) * 0.6
         }
+        
         cf3 <- function(x) {
+            (0.11 + exp(-(abs(x[adj]) * 100 + 5)^2/(2 * 30^2)))
+        }
+        
+        cf4 <- function(x) {
+            (0.11 + 0.67 * exp(-(abs(x[adj]) * 100 + 2)^2/(2 * 30^2)))
+        }
+        
+        cf5 <- function(x) {
+            0.95 * (0.11 + exp(-(abs(x[adj]) * 100 + 5)^2/(2 * 30^2)))
+        }
+        
+        cf6 <- function(x) {
+            0.95 * (0.11 + 0.67 * exp(-(abs(x[adj]) * 100 + 2)^2/(2 * 30^2)))
+        }
+        
+        cf7 <- function(x) {
             (4.8 * exp(-5.3 * abs((x[adj] * 0.7) + 0.03)))
         }
-        cf4 <- function(x) {
+        cf8 <- function(x) {
             (1/(1 + abs(x[adj] * 100/crit_slope)^2))
         }
-        cf5 <- function(x) {
+        cf9 <- function(x) {
             (1/((1337.8 * x[adj]^6) + (278.19 * x[adj]^5) - (517.39 * x[adj]^4) - (78.199 * x[adj]^3) + (93.419 * x[adj]^2) + (19.825 * x[adj]) + 1.64))
         }
-        cf6 <- function(x) {
+        cf10 <- function(x) {
             (1/(2.635 + (17.37 * abs(x[adj])) + (42.37 * abs(x[adj])^2) - (21.43 * abs(x[adj])^3) + (14.93 * abs(x[adj])^4)))
         }
         
@@ -172,6 +224,10 @@ create_slope_cs <- function(dem, cost_function = "tobler", neighbours = 16, crit
         slope_stack[[4]][adj] <- cf4(slope)
         slope_stack[[5]][adj] <- cf5(slope)
         slope_stack[[6]][adj] <- cf6(slope)
+        slope_stack[[7]][adj] <- cf7(slope)
+        slope_stack[[8]][adj] <- cf8(slope)
+        slope_stack[[9]][adj] <- cf9(slope)
+        slope_stack[[10]][adj] <- cf10(slope)
         
         if (inherits(max_slope, "numeric")) {
             slope_stack[[1]]@transitionMatrix@x[slope_stack[[1]]@transitionMatrix@x == cf1(rand)] <- 0
@@ -180,6 +236,10 @@ create_slope_cs <- function(dem, cost_function = "tobler", neighbours = 16, crit
             slope_stack[[4]]@transitionMatrix@x[slope_stack[[4]]@transitionMatrix@x == cf4(rand)] <- 0
             slope_stack[[5]]@transitionMatrix@x[slope_stack[[5]]@transitionMatrix@x == cf5(rand)] <- 0
             slope_stack[[6]]@transitionMatrix@x[slope_stack[[6]]@transitionMatrix@x == cf6(rand)] <- 0
+            slope_stack[[7]]@transitionMatrix@x[slope_stack[[6]]@transitionMatrix@x == cf7(rand)] <- 0
+            slope_stack[[8]]@transitionMatrix@x[slope_stack[[8]]@transitionMatrix@x == cf8(rand)] <- 0
+            slope_stack[[9]]@transitionMatrix@x[slope_stack[[9]]@transitionMatrix@x == cf9(rand)] <- 0
+            slope_stack[[10]]@transitionMatrix@x[slope_stack[[10]]@transitionMatrix@x == cf10(rand)] <- 0
         }
         
         
