963159799a39798ec0b7d5e65942e2304d852fe4
Joseph <Josephlewis1992@gmail.com>
2020-05-13 18:01:12 +0100

    formatting

--- R/cost_matrix.R
+++ R/cost_matrix.R
@@ -39,44 +39,44 @@
 #'nb_matrix = matrix, cost_distance = FALSE, parallel = FALSE)
 
 cost_matrix <- function(cost_surface, locations, k) {
-
+    
     if (!inherits(cost_surface, "TransitionLayer")) {
         stop("cost_surface argument is invalid. Expecting a TransitionLayer object")
     }
-
+    
     if (!inherits(locations, c("SpatialPoints", "SpatialPointsDataFrame"))) {
         stop("Locations argument is invalid. Expecting SpatialPoints or SpatialPointsDataFrame object")
     }
-
+    
     if (!inherits(k, "numeric")) {
         stop("k argument is invalid. Expecting numeric object")
     }
-
+    
     if (length(locations) < 2) {
         stop("Number of locations invalid. Expecting more than one location")
     }
-
+    
     if (k > length(locations) - 1) {
         stop("k Value exceeds number of locations that each location can connect to. See details for more information.")
     }
-
+    
     origin <- rep(1:length(locations), each = k)
     destination <- rep(0, length(locations), each = k)
-
+    
     for (i in 1:length(locations)) {
-
+        
         distances <- costDistance(x = cost_surface, fromCoords = locations[i, ], toCoords = locations[-i, ])
-
+        
         distances <- data.frame(ID = 2:length(locations), costDistance = as.vector(distances))
-
+        
         distances <- distances[order(distances$costDistance), ]
-
+        
         destination[which(origin == i)] <- distances$ID[1:k]
-
+        
     }
-
+    
     matrix <- matrix(data = c(origin, destination), ncol = 2)
-
+    
     return(matrix)
-
+    
 }
