ff666c1644708784d829d42e7577b06ab0c41d16
Joseph <Josephlewis1992@gmail.com>
2020-06-05 11:28:42 +0100

    32 and 48 neighbourhood matrices documentation

--- R/neighbours.R
+++ R/neighbours.R
@@ -1,21 +1,30 @@
-#' 32 Neighbourhood matrix
+#' 32 Neighbourhood matrices based on Kovanen and Sarjakoski (2015)
 #'
-#' 32 neighbourhood matrix based on [Kovanen and Sarjakoski (2015)](https://doi.org/10.1145/2803172)
+#' see leastcostpath::neighbours_32 for layout
 #'
 #' @export
 #'
 #' @author Joseph Lewis
+#'
+#' @references
+#'
+#' Kovanen, J., Sarjakoski, T. (2015). Tilewise Accumulated Cost Surface Computation with Graphics Processing Units. ACM Transactions on Spatial Algorithms and Systems 1, 1-27. \url{https://doi.org/10.1145/2803172}
+#'
 
 neighbours_32 <- matrix(c(NA, 1, 1, NA, 1, 1, NA, 1, NA, 1, NA, 1, NA, 1, 1, 1, 1, 1, 1, 1, 1, NA, NA, 1, 0, 1, NA, NA, 1, 1, 1, 1, 1, 1, 1, 1, NA, 1, NA, 1, NA, 
     1, NA, 1, 1, NA, 1, 1, NA), nrow = 7, ncol = 7, byrow = TRUE)
 
-#' 48 Neighbourhood matrix
+#' 48 Neighbourhood matrices based on Kovanen and Sarjakoski (2015)
 #'
-#' 48 neighbourhood matrix based on [Kovanen and Sarjakoski (2015)](https://doi.org/10.1145/2803172)
+#' see leastcostpath::neighbours_48 for layout
 #'
 #' @export
 #'
 #' @author Joseph Lewis
+#'
+#' @references
+#'
+#' Kovanen, J., Sarjakoski, T. (2015). Tilewise Accumulated Cost Surface Computation with Graphics Processing Units. ACM Transactions on Spatial Algorithms and Systems  1, 1-27. \url{https://doi.org/10.1145/2803172}
 
 neighbours_48 <- matrix(c(NA, 1, NA, 1, NA, 1, NA, 1, NA, 1, NA, 1, 1, NA, 1, 1, NA, 1, NA, 1, NA, 1, NA, 1, NA, 1, NA, 1, 1, 1, 1, 1, 1, 1, 1, 1, NA, NA, NA, 1, 
     0, 1, NA, NA, NA, 1, 1, 1, 1, 1, 1, 1, 1, 1, NA, 1, NA, 1, NA, 1, NA, 1, NA, 1, NA, 1, 1, NA, 1, 1, NA, 1, NA, 1, NA, 1, NA, 1, NA, 1, NA), nrow = 9, ncol = 9, 
