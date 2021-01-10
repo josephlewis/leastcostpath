07e30ea79fe47677b70aa26e1a60db13414e1a5c
Joseph <Josephlewis1992@gmail.com>
2021-01-10 22:52:01 +0000

    Version 1.7.9

--- R/transitions.R
+++ R/transitions.R
@@ -12,26 +12,16 @@
 #' @author Joseph Lewis
 
 transition_slope <- function(x, neighbours) {
-
-    tr <- new("TransitionLayer", nrows = as.integer(nrow(x)), ncols = as.integer(ncol(x)), extent = extent(x), crs = projection(x, asText = FALSE), transitionMatrix = Matrix(0,
+    
+    tr <- new("TransitionLayer", nrows = as.integer(nrow(x)), ncols = as.integer(ncol(x)), extent = extent(x), crs = projection(x, asText = FALSE), transitionMatrix = Matrix(0, 
         ncell(x), ncell(x)), transitionCells = 1:ncell(x))
     transitionMatr <- transitionMatrix(tr)
     Cells <- which(!is.na(getValues(x)))
-
-    if (inherits(neighbours, "numeric")) {
-        if (neighbours == 32) {
-            neighbours_matrix <- neighbours_32
-
-        } else if (neighbours == 48) {
-            neighbours_matrix <- neighbours_48
-        }
-
-    }
-
-    adj <- raster::adjacent(x, cells = Cells, pairs = TRUE, target = Cells, directions = neighbours_matrix)
-
+    
+    adj <- raster::adjacent(x, cells = Cells, pairs = TRUE, target = Cells, directions = neighbours)
+    
     transition.values <- getValues(x)[adj[, 2]] - getValues(x)[adj[, 1]]
-
+    
     transitionMatr[adj] <- as.vector(transition.values)
     transitionMatrix(tr) <- transitionMatr
     matrixValues(tr) <- "conductance"
