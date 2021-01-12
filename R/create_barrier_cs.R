d3f322b7c328a23caa1e7e6905425998704d7664
Joseph <Josephlewis1992@gmail.com>
2021-01-12 22:12:50 +0000

    create_barrier_cs and crop_cs now allow SpatialPoints, SpatialLines, SpatialPolygons, and RasterLayer objects as barriers/boundaries

--- R/create_barrier_cs.R
+++ R/create_barrier_cs.R
@@ -4,11 +4,11 @@
 #'
 #' @details
 #'
-#' The resultant Barrier Cost Surface is produced by assessing which areas of the raster coincide with the SpatialPolygons as specified in the barrier argument. The areas of the raster that coincide with the SpatialPolygons are given a conductance value of 0 (default value), with all other areas given a Conductance value of 1 (default value). The conductance value of 0 ensures that movement is inhibited within these areas. Examples of use include rivers, altitudes, and taboo areas.
+#' The resultant Barrier Cost Surface is produced by assessing which areas of the raster coincide with the Spatial* or RasterLayer object as specified in the barrier argument. The areas of the raster that coincide with the barrier are given a conductance value of 0 (default value, with all other areas given a Conductance value of 1 (default value). The conductance value of 0 ensures that movement is inhibited within these areas. Examples of use include rivers, altitudes, and taboo areas. If a RasterLayer object is supplied in the barrier argument then all cells with a value NOT \code{NA} will be used as the barrier.
 #'
 #' @param raster \code{RasterLayer} (raster package). The Resolution, Extent, and Spatial Reference System of the provided RasterLayer is used when creating the resultant Barrier Cost Surface
 #'
-#' @param barrier \code{SpatialPolygons} (sp package). Area within the landscape that movement is inhibited. See details for more
+#' @param barrier \code{Spatial*} (sp package) or \code{RasterLayer} (raster package). Area within the landscape that movement is inhibited. See details for more
 #'
 #' @param neighbours \code{numeric} value. Number of directions used in the Least Cost Path calculation. See Huber and Church (1985) for methodological considerations when choosing number of neighbours. Expected numeric values are 4, 8, 16, 32, 48 or a matrix object. Default is numeric value 16
 #'
@@ -31,12 +31,22 @@
 #'
 #' @examples
 #' r <- raster::raster(system.file('external/maungawhau.grd', package = 'gdistance'))
-#' loc1 = cbind(2667670, 6479000)
-#' loc1 = sp::SpatialPoints(loc1)
 #'
-#' loc1 <- rgeos::gBuffer(loc1, width = 50)
+#' pt = cbind(2667670, 6479000)
+#' pt = sp::SpatialPoints(pt)
+#' polygon <- rgeos::gBuffer(spgeom = pt, width = 200)
+#' raster::crs(pt) <- raster::crs(r)
+#' raster::crs(polygon) <- raster::crs(r)
 #'
-#' barrier <- create_barrier_cs(raster = r, barrier = loc1)
+#' barrier_pt <- create_barrier_cs(raster = r, barrier = pt)
+#' barrier_polygon <- create_barrier_cs(raster = r, barrier = polygon)
+#'
+#' r2 <- r
+#' ext <- raster::extent(2667500, 2667900, 6478800, 6479500)
+#' cells <- unlist(raster::cellFromPolygon(object = r, p = as(ext, 'SpatialPolygons')))
+#' r2[-cells] <- NA
+#'
+#' barrier_ras <- create_barrier_cs(raster = r, barrier = r2)
 
 create_barrier_cs <- function(raster, barrier, neighbours = 16, field = 0, background = 1) {
     
@@ -44,8 +54,8 @@ create_barrier_cs <- function(raster, barrier, neighbours = 16, field = 0, backg
         stop("raster argument is invalid. Expecting a RasterLayer object")
     }
     
-    if (!inherits(barrier, "SpatialPolygons")) {
-        stop("barrier argument is invalid. Expecting a Spatial* object")
+    if ((!inherits(barrier, "Spatial")) & (!inherits(barrier, "RasterLayer"))) {
+        stop("barrier argument is invalid. Expecting a Spatial* or RasterLayer object")
     }
     
     if (any(!neighbours %in% c(4, 8, 16, 32, 48)) & (!inherits(neighbours, "matrix"))) {
@@ -66,8 +76,23 @@ create_barrier_cs <- function(raster, barrier, neighbours = 16, field = 0, backg
     barrier_cs <- new("TransitionLayer", nrows = as.integer(nrow(raster)), ncols = as.integer(ncol(raster)), extent = extent(raster), crs = projection(raster, 
         asText = FALSE), transitionMatrix = Matrix(0, ncell(raster), ncell(raster)), transitionCells = 1:ncell(raster))
     
-    # get cells that coincide with barrier
-    barrier_cells <- raster::cellFromPolygon(object = raster, p = barrier)[[1]]
+    if (inherits(barrier, "SpatialPoints")) {
+        
+        barrier_cells <- raster::cellFromXY(object = raster, xy = barrier)
+        
+    } else if (inherits(barrier, "SpatialLines")) {
+        
+        barrier_cells <- unlist(raster::cellFromLine(object = raster, lns = barrier))
+        
+    } else if (inherits(barrier, "SpatialPolygons")) {
+        
+        barrier_cells <- unlist(raster::cellFromPolygon(object = raster, p = barrier))
+        
+    } else if (inherits(barrier, "RasterLayer")) {
+        
+        barrier_cells <- which(!is.na(raster::getValues(barrier)))
+        
+    }
     
     # get adjacent cells to limit value change
     adj <- raster::adjacent(raster, cells = 1:raster::ncell(raster), pairs = TRUE, directions = neighbours)
@@ -84,3 +109,4 @@ create_barrier_cs <- function(raster, barrier, neighbours = 16, field = 0, backg
     return(barrier_cs)
     
 }
+
