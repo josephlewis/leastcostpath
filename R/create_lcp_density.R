04738e2380c527b6d76ddd43e98d3c796937baf3
Joseph <Josephlewis1992@gmail.com>
2022-08-30 17:24:16 +0100

    add examples

--- R/create_lcp_density.R
+++ R/create_lcp_density.R
@@ -13,23 +13,43 @@
 #' @author Joseph Lewis
 #' 
 #' @export
+#' 
+#' @examples 
+#' 
+#' r <- terra::rast(system.file("extdata/SICILY_1000m.tif", package="leastcostpath"))
+#' 
+#' slope_cs <- create_slope_cs(x = r, cost_function = "tobler", neighbours = 4)
+#' 
+#' locs <- sf::st_sf(geometry = sf::st_sfc(
+#' sf::st_point(c(839769, 4199443)),
+#' sf::st_point(c(1038608, 4100024)),
+#' sf::st_point(c(907695, 4145478)),
+#' sf::st_point(c(1054446, 4232288)),
+#' sf::st_point(c(957989, 4208863)),
+#' crs = terra::crs(r)))
+#' 
+#' lcps <- create_FETE_lcps(x = slope_cs, locations = locs, cost_distance = TRUE)
+#' 
+#' lcps_dens <- create_lcp_density(x = r, lcps = lcps, rescale = TRUE)
 
 create_lcp_density <- function(x, lcps, rescale = FALSE) {
   
-  cs_rast <- terra::rast(nrow = x$nrow, ncol = x$ncol, extent = x$extent, crs = x$crs)
-  cs_rast <- terra::setValues(cs_rast, 0)
+  ras <- x
+  ras <- terra::setValues(ras, 0)
   
   lcps_vect <- terra::vect(lcps)
   
-  cum_rast <- terra::rasterizeGeom(x = lcps_vect, y = cs_rast, fun = "crosses")
+  cumul_ras <- terra::rasterizeGeom(x = lcps_vect, y = ras, fun = "crosses")
 
   if(rescale) { 
-    rast_min <- terra::minmax(cum_rast)[1]
-    rast_max <- terra::minmax(cum_rast)[2]
+    rast_min <- terra::minmax(cumul_ras)[1]
+    rast_max <- terra::minmax(cumul_ras)[2]
     
-    cum_rast <- ((cum_rast - rast_min)/(rast_max - rast_min)) 
+    cumul_ras <- ((cumul_ras - rast_min)/(rast_max - rast_min)) 
   }
   
-  return(cum_rast)
+  cumul_ras[is.na(x)] <- NA
+  
+  return(cumul_ras)
   
 }
